82668b09981e36946d247f8e8137ec0c3305216d
paulkoch <paulkoch95@googlemail.com>
2023-02-07 18:18:26 +0100

    last fixes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 # MacOS Specific Fix, not necessary on version < BigSur and other Posix
-# os.environ['QT_MAC_WANTS_LAYER'] = '1'
+os.environ['QT_MAC_WANTS_LAYER'] = '1'
 
-from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QShortcut, QWidget, QVBoxLayout, QLineEdit, QMenu, QGridLayout, QAction, QLayout
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QShortcut, QWidget, QVBoxLayout, QLineEdit, QMenu, QGridLayout, QAction, QLayout, QCompleter
 from PySide2.QtGui import QFont, QKeySequence, QCursor, QIcon
 from PySide2.QtCore import Qt, Slot, QDir, QFileInfo
 import sys
@@ -18,9 +18,9 @@ class RichContextMenu(QWidget):
         super(RichContextMenu, self).__init__(parent)
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.options = []
-        self.block_icons = self.load_icons("./total_icons/blocks")
-        self.node_icons = self.load_icons("./total_icons/nodes")
-        self.all_icons = self.load_icons("./total_icons/all")
+        # self.all_icons = self.load_icons(["./total_icons"])
+        self.loaded_icons = self.load_icons_dict("./total_icons")
+
         self.layout = QGridLayout()
         self.layout.setSpacing(0)
 
@@ -36,6 +36,15 @@ class RichContextMenu(QWidget):
                                     padding: 5px;
                                 }
                                 """
+
+        ##################################
+        # Populate Context Menue Entries #
+        ##################################
+        menu = {"Blocks": ["arm", "leg", "spine", "root"], "Post Build Nodes": ["constraint", "autoWeights"], "Arbitrary Nodes": ["hand", "flateye", "brow"]}
+        self.options = [item for sublist in menu.values() for item in sublist]
+        print("All options: ", self.options)
+        self.build_dynamic_menu(menu)
+
         ####################
         # Setup Search bar #
         ####################
@@ -44,17 +53,19 @@ class RichContextMenu(QWidget):
         self.search_menu.setStyleSheet("QLineEdit { border: 2px solid black; background-color: white;}")
         self.search_menu.textChanged[str].connect(self.search_results)
 
-        ##################################
-        # Populate Context Menue Entries #
-        ##################################
-        self.load_menue_structure()
+        self.text_completer = QCompleter([lit.capitalize() for lit in self.options], self)
+        print("opt", self.options)
+        self.text_completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.search_menu.setCompleter(self.text_completer)
+
 
         ########################################
         # Setup Extra Menue for Search results #
         ########################################
         self.search_results_menu = QMenu()
         self.search_results_menu.setStyleSheet(self.menue_style_sheet)
-        self.search_results_menu.triggered.connect(self.act_on_action)
+        self.search_results_menu.triggered.connect(self.act_action)
+
         ################
         # Layut Widget #
         ################
@@ -67,76 +78,92 @@ class RichContextMenu(QWidget):
         self.setLayout(self.layout)
         self.setFixedSize(self.layout.sizeHint())
 
-    def load_menue_structure(self) -> None:
+    def build_dynamic_menu(self, menu_struct: dict[str, list[str]]):
+        print("loaded icons", self.loaded_icons)
+
         self.primary_menue = QMenu()
         self.primary_menue.setStyleSheet(self.menue_style_sheet)
-
         self.primary_menue.aboutToHide.connect(lambda: self.prevent_deletion(self.primary_menue))
+        #
 
-        self.primary_menue.triggered.connect(self.act_on_action)
+        all_actions = []
 
-        self.sub_m = QMenu(self.primary_menue)
-        self.sub_m.aboutToHide.connect(lambda: self.sub_prevent_closing(self.sub_m))
+        for title, submenu in menu_struct.items():
+            m_sub = QMenu(self.primary_menue)
+            m_sub.setTitle(title)
+            m_sub.triggered.connect(self.act_action)
 
-        self.sub_m.setTitle("Blocks")
-        self.blocks_menue_items = self.block_icons
-        self.sub_m.addActions([QAction(action_icon[0], str(action_icon[1]), self.primary_menue) for action_icon in self.blocks_menue_items])
+            opt = []
+            for entry in submenu:
+                if entry != "*":
+                    if entry in self.loaded_icons.keys():
+                        temp_act = QAction(self.loaded_icons.get(entry).get("icon"), entry.capitalize(), self.primary_menue)
+                        m_sub.addAction(temp_act)
+                        all_actions.append(temp_act)
 
-        self.sub_menue_nodes = QMenu(self.primary_menue)
-        self.sub_menue_nodes.aboutToHide.connect(lambda: self.sub_prevent_closing(self.sub_menue_nodes))
+                    else:
+                        temp_act = QAction(QIcon(), entry.capitalize(), self.primary_menue)
+                        m_sub.addAction(temp_act)
+                        all_actions.append(temp_act)
 
-        self.sub_menue_nodes.setTitle("Post Build Nodes")
-        self.sub_menue_nodes_items = self.node_icons
-        self.sub_menue_nodes.addActions([QAction(action_icon[0], str(action_icon[1]), self.primary_menue) for action_icon in self.sub_menue_nodes_items])
+            m_sub.updateGeometry()
+            self.primary_menue.addMenu(m_sub)
 
-        self.sub_menue_all = QMenu(self.primary_menue)
-        self.sub_menue_all.aboutToHide.connect(lambda: self.sub_prevent_closing(self.sub_menue_all))
+            m_sub.setStyleSheet("""QMenu:item {margin-left: 8px;}""")
 
-        self.sub_menue_all.setTitle("All")
-        self.sub_menue_all.addActions([QAction(action_icon[0], str(action_icon[1]), self.primary_menue) for action_icon in self.all_icons])
-
-        self.sub_menue_all.setContentsMargins(5, 0, 0, 0)
-
-        self.primary_menue.addMenu(self.sub_m)
-        self.primary_menue.addMenu(self.sub_menue_nodes)
-        self.primary_menue.addSeparator()
-        self.primary_menue.addMenu(self.sub_menue_all)
         self.primary_menue.setFixedWidth(150)
-
-        self.sub_menue_nodes.setStyleSheet("""QMenu:item {margin-left: 8px;}""")
-        self.sub_m.setStyleSheet("""QMenu:item {margin-left: 8px;}""")
-        self.sub_menue_all.setStyleSheet("""QMenu:item {margin-left: 8px;}""")
+        self.primary_menue.addSeparator()
+        self.all_menu = QMenu(self.primary_menue)
+        self.all_menu.setTitle("All")
+        self.all_menu.addActions(all_actions)
+        self.all_menu.setFixedWidth(150)
+        self.primary_menue.addMenu(self.all_menu)
 
     def showEvent(self, event):
         self.search_menu.setText("")
         super().showEvent(event)
 
-    @Slot(QAction)
-    def act_on_action(self, action: QAction) -> None:
+    # @Slot(QAction)
+    def act_action(self, action: QAction) -> None:
         """ Here you can add logic for what to do when an entry was clicked. """
         self.setVisible(False)
         self.primary_menue.setVisible(True)
-
         print("Entry was clicked!", action.text())
 
-    def load_icons(self, dir: str) -> list[list[QIcon, str]]:
+    def load_icons_dict(self, dir):
+        icons = {}
+        icon_dir = QDir(dir)
+        icon_files = icon_dir.entryInfoList(["*.png"])
+
+        file_info: QFileInfo
+        for file_info in icon_files:
+            file_path = file_info.absoluteFilePath()
+            icons[file_info.fileName().split(".")[0].lower()] = {
+                "icon" : QIcon(file_path)
+            }
+        # self.options = icons.keys()
+        return icons
+    def load_icons(self, dir: list[str]) -> list[list[QIcon, str]]:
         """ Load images and extract entry names from Image Names"""
-        self.icon_dir = QDir(dir)
-        self.icon_files = self.icon_dir.entryInfoList(["*.png"])
 
         icons = []
-        file_info: QFileInfo
-        for file_info in self.icon_files:
-            file_path = file_info.absoluteFilePath()
-            icons.append([QIcon(file_path), file_info.fileName().split(".")[0].capitalize() ])
-        self.options = [item[1] for item in icons]
-        print(self.options)
+        for directory in dir:
+            icon_dir = QDir(directory)
+            icon_files = icon_dir.entryInfoList(["*.png"])
+
+            dir_icons = []
+            file_info: QFileInfo
+            for file_info in icon_files:
+                file_path = file_info.absoluteFilePath()
+                dir_icons.append([QIcon(file_path), file_info.fileName().split(".")[0].capitalize()])
+            icons.extend(dir_icons)
         return icons
 
     @Slot(str)
     def search_results(self, text: str) -> None:
         """ Search in loaded menue entry options using SequenceMatching """
         res = difflib.get_close_matches(text, self.options)
+
         if len(res)==0:
             self.search_results_menu.setVisible(False)
             return
@@ -144,10 +171,11 @@ class RichContextMenu(QWidget):
             self.search_results_menu.setVisible(True)
 
         self.search_results_menu.clear()
-        acts = self.filter_icon_list_by_name(self.icons, res)
-
-        for opt in acts:
-            self.search_results_menu.addAction(opt[0], opt[1])
+        for result in res:
+            if result in self.loaded_icons:
+                self.search_results_menu.addAction(self.loaded_icons.get(result).get("icon"), result.capitalize())
+            else:
+                self.search_results_menu.addAction(QIcon(), result.capitalize())
 
         self.updateGeometry()
         self.search_results_menu.updateGeometry()
@@ -163,7 +191,7 @@ class RichContextMenu(QWidget):
 
     def filter_icon_list_by_name(self, icon_list: list[list[QIcon, str]], filter: list[str]):
         """ Find QIcon in List of QIcons and Strings based on List of desired results """
-        return [opt for opt in icon_list if opt[1] in filter]
+        return [opt for opt in icon_list if opt[1].lower() in filter]
 
 class Window(QMainWindow):
     def __init__(self):
